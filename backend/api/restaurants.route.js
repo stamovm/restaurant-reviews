e6ae26e9ed937cebca4fc07fb03bfb39b2dc5fb6
stamovm <m.stamov@yahoo.com>
2022-05-04 23:10:26 -0700

    Added reviewsDAO and reviews controler at the backend.

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -1,6 +1,15 @@
 import express from 'express'
 import RestaurantsCtrl from './restaurants.controller.js'
+import ReviewsCtrl from './reviews.controller.js'
+
 const router = express.Router()
 // router.route('/').get((req, res) => res.send('<h1>hello world</h1>'))
 router.route('/').get(RestaurantsCtrl.apiGetRestaurants)
+
+router
+  .route('/review')
+  .post(ReviewsCtrl.apiPostReview)
+  .put(ReviewsCtrl.apiUpdateReview)
+  .delete(ReviewsCtrl.apiDeleteReview)
+
 export default router
