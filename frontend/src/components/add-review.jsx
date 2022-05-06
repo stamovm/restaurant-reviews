744bc60984ec640aba0989f393044c8ab061022c
stamovm <m.stamov@yahoo.com>
2022-05-05 23:54:34 -0700

    Updated frontend app.js. Created files for some of the components.

---
+++ frontend/src/components/add-review.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function AddReview() {
+  return <div>AddReview</div>
+}
---
+++ frontend/src/components/restaurants-list.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function RestaurantsList() {
+  return <div>RestaurantsList</div>
+}
